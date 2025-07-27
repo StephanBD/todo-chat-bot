c24d3cfff072c47fe550a570dfb9bc7186669a02
StephanBD <50586286+StephanBD@users.noreply.github.com>
2025-07-27 08:21:11 -0500

    Initial commit: Set up Vue.js project

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import './assets/main.css'
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+// ------------------
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import vuetify from './plugins/vuetify'
+// ------------------
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(vuetify)
+// ------------------
+app.mount('#app')
